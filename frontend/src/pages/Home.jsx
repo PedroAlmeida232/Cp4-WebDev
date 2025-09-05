70f0dd06779617771ef326adb148fab9ea8e8410
Luiz <nandoluizprimeiro@gmail.com>
2025-09-04 21:47:24 -0300

    Adicionando banner

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,23 +1,23 @@
+import bannerImage from "../assets/banner.png";
+import '../styles/Home.css'; 
+
 function Home() {
   return (
     <div id="banner-principal">
-        {/* Aqui deve conter uma imagem de fundo meio borrada  */}
-        <img src="" alt="" id="imagem-banner"/> 
-        <div id="titulo-banner">
-            <h1>
-                Encontre o Carro dos Seus Sonhos
-            </h1>
-        </div>
-        <div id="subtitulo-banner">
-            <p>
-                Descubra nossa seleção premium de veículos novos e seminovos. Qualidade garantida e os melhores preços do mercado.
-            </p>
-        </div>
-        <div id="botao-banner">
-            <button>Saiba mais</button>
-        </div>
+      <div id="titulo-banner">
+        <h1>
+          Encontre o Carro dos Seus Sonhos
+        </h1>
+      </div>
+      <div id="subtitulo-banner">
+        <p>
+          Descubra nossa seleção premium de veículos novos e seminovos. Qualidade garantida e os melhores preços do mercado.
+        </p>
+      </div>
+      <div id="botao-banner">
+        <button>Saiba mais</button>
+      </div>
     </div>
-    
   );
 }
 
