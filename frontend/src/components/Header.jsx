abab7ca126acdaba1a906e6e6d2adc52d799edbf
Luiz <nandoluizprimeiro@gmail.com>
2025-09-05 16:21:09 -0300

    Estilizando o Header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,66 +1,59 @@
 import { useState } from "react";
-import logo from "../assets/logo.png";
+import logo from "../assets/insight-logo.png";
 import "../styles/Header.css";
 
 function Header() {
-  const [mensagem, setMensagem] = useState("Nenhuma Mensagem do Servidor");
+  const [mensagem, setMensagem] = useState(".");
   const [endpointAtual, setEndPointAtual] = useState("");
-  const [status, setStatus] = useState("idle"); // Estado inicial 'idle'
+  const [status, setStatus] = useState("idle");
 
   const busca = async (api) => {
     setEndPointAtual(api);
     setMensagem("Carregando...");
-    setStatus("loading"); // Alterado para 'loading'
+    setStatus("loading");
 
     try {
       const resposta = await fetch(`http://localhost:5001${api}`);
       if (!resposta.ok) {
         console.log(`Erro na requisição ${resposta.status}`);
         setMensagem(`Erro na requisição: ${resposta.status}`);
-        setStatus("error"); // Alterado para 'error'
+        setStatus("error");
         return;
       }
       const data = await resposta.text();
       setMensagem(data);
-      setStatus("success"); // Alterado para 'success'
+      setStatus("success");
     } catch (error) {
       console.log(`Erro ao buscar dados: ${error}`);
       setMensagem(`Erro ao conectar ao servidor: ${error.message}`);
-      setStatus("error"); // Alterado para 'error'
+      setStatus("error");
     }
   };
 
   return (
     <header>
-      <div>
-        <img src={logo} alt="logo" />
+      <div className="logo-container">
+        <a href="/">
+          <img src={logo} alt="Logo da Insight Carros" />
+          <span className="logo-text">Insight</span>
+        </a>
       </div>
       <nav>
         <ul>
           <li>
-            <a onClick={() => busca("/carros")}
-              href="/carros"
-            >Carros</a>
+            <a onClick={() => busca("/carros")} href="/carros">Carros</a>
           </li>
           <li>
-            <a onClick={() => busca("/loja")}
-              href="/loja"
-            >Loja</a>
+            <a onClick={() => busca("/loja")} href="/loja">Loja</a>
           </li>
           <li>
-            <a onClick={() => busca("/sobre")}
-              href="/sobre"
-            >Sobre</a>
+            <a onClick={() => busca("/sobre")} href="/sobre">Sobre</a>
           </li>
           <li>
-            <a onClick={() => busca("/contato")}
-              href="/contato"
-            >Contato</a>
-          </li>
-          <li>
-            <div className={`status-indicator ${status}`}></div>
+            <a onClick={() => busca("/contato")} href="/contato">Contato</a>
           </li>
         </ul>
+        <p className={`status-text ${status}`}>{mensagem}</p>
       </nav>
     </header>
   );
