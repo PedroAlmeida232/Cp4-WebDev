a98d7a207e49ff001aaee17d2081b5e1e3e5c4f1
Luiz <nandoluizprimeiro@gmail.com>
2025-09-05 10:04:46 -0300

    Fazendo a troca de componentes por página na renderização

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,18 +6,33 @@ import Loja from "./components/Loja"
 import Sobre from "./components/Sobre"
 
 function App() {
+  const caminhoAtual = window.location.pathname;
+
+  const renderizarPagina = () => {
+    switch (caminhoAtual) {
+      case "/carros":
+        return <Carros />;
+      case "/loja":
+        return <Loja />;
+      case "/sobre":
+        return <Sobre />;
+      case "/contato":
+        return <Contato />;
+      default:
+        
+        return <div>Página não encontrada</div>;
+    }
+  };
+
   return (
     <>
       <Header />
       <main>
-        <Carros />
-        <Contato />
-        <Loja />
-        <Sobre />
+        {renderizarPagina()}
       </main>
       <Footer />
     </>
   );
 }
 
-export default App
+export default App;
